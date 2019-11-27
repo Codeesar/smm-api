35cbdf00e63aa58e3661ee2e9976a785f8c84bce
kefs <32211521+Codeesar@users.noreply.github.com>
2019-11-27 23:35:57 +1000

    updated to 2.0.0 BETA

--- main.cpp
+++ main.cpp
@@ -22,15 +22,24 @@
 #include "main.h"
 #include <math.h>
 
-#define API_VERSION         "1.1.0"
-#define SMM_VERSION         "0.0.0.6"
+#define API_VERSION         "2.0.0-BETA"
+#define SMM_VERSION         "0.0.0.7"
 
-#define CORE_PROTOCOL       96
+#define CORE_PROTOCOL       207
+
 #define INVALID_CLASS       0xFFF
 #define UNAVAILABLE_API     0xFFF
+
 #define M_PI                3.14159265358979323846
 
 // ------------------------------------------------------------------------------------------------------ //
+
+uint32_t m_bProtocolBack = 0;
+uint32_t m_bProtocolBackMin = 0;
+uint32_t m_bProtocolBackMax = 0;
+uint32_t m_bProtocolBackFinal = 0;
+
+uintptr_t lib_ptr = nullptr;
 
 char* GetAPIVersion()
 {
@@ -47,352 +56,59 @@ int GetCoreProtocol()
     return CORE_PROTOCOL;
 }
 
-uint32_t m_bProtocolBack = 0;
-uint32_t m_bProtocolBackMin = 0;
-uint32_t m_bProtocolBackMax = 0;
-uint32_t m_bProtocolBackFinal = 0;
 void InitProtocol()
 {
-    m_bProtocolBack = ((atan(((uint32_t)GetCoreProtocol()) / M_PI * 4.25) + 256) / 0.005);
-    m_bProtocolBackMin = m_bProtocolBack / ((uint32_t)GetCoreProtocol());
-    m_bProtocolBackMax = m_bProtocolBack * ((uint32_t)GetCoreProtocol());
-    m_bProtocolBackFinal = m_bProtocolBackMax - m_bProtocolBackMin;
+    m_bProtocolBack 		= (GetCoreProtocol() + 0xF4) >> 64;
+    m_bProtocolBackMin 		= (GetCoreProtocol() + 0xF8) >> 64;
+    m_bProtocolBackMax 		= (GetCoreProtocol() + 0x12) >> 64;
+    m_bProtocolBackFinal 	= (GetCoreProtocol() + 0x16) >> 64;
 }
 
 // ------------------------------------------------------------------------------------------------------ //
 
-/*
- * @return CCheat
-*/
-CCheat* CCheat::GetClass()
+uintptr_t getSMM()
 {
-    InitProtocol();   
-
-    if(GetAPIVersion() == UNAVAILABLE_API or m_bProtocolBackFinal <= 0) 
-        return ((CCheat*) INVALID_CLASS);
-
-    return ((CCheat*) this);
+	return ((const uint32_t*)lib_ptr); // FindLibrary("libsamp_original.so") .. etc..
 }
 
-// Main loop of CCheat::class
-void CCheat::Process()
+// ------------------------------------------------------------------------------------------------------ //
+
+// todo
+
+/* addresses */ 
+#define ADDR_CCHEAT_CLASS 				0x156E24
+#define ADDR_CCHEAT_ADDMENUITEM 		0x156E70
+#define ADDR_CCHEAT_REMOVEMENUITEM 		0x156E7C
+#define ADDR_CCHEAT_DRAWCHEATBOX 		0x156EC4
+#define ADDR_CCHEAT_DRAW 				0x156EF6
+
+// ccheat class funcs ::
+void ccheat__cheat()
 {
-    CCheat::GetClass()->Process();
+	return (( void (*)())(getSMM()+ADDR_CCHEAT_CLASS+1))();
 }
 
-/*
- * @return float
-*/
-float CCheat::GetNoClipSpeed()
+void ccheat__menu_item_add(char* itemName, int funcId, int menuType)
 {
-    return ((float)CCheat::GetClass()->m_bNoClipSpeed);
+	// ADDR_CCHEAT_ADDMENUITEM + 0x6; 	// color
+	// ADDR_CCHEAT_ADDMENUITEM + 0x22; 	// function switcher
+
+	// code is executed in the same function by calling SetMenuState() and SetCheatState()
+
+	return (( void (*)(char*, int, int))(getSMM()+ADDR_CCHEAT_ADDMENUITEM+1))(itemName, fundId, menuType);
 }
 
-/*
- * @param float speed
-*/
-void CCheat::SetNoClipSpeed(float speed)
+void ccheat__menu_item_remove(int itemId)
 {
-    CCheat::GetClass()->m_bNoClipSpeed = speed;
-
-    return;
+	return (( void (*)(int))(getSMM()+ADDR_CCHEAT_REMOVEMENUITEM+1))(itemId);
 }
 
-void CCheat::TPLook()
+void ccheat__DrawCheatBox()
 {
-    return CCheat::GetClass()->TPLook();
+	return (( void (*)())(getSMM()+ADDR_CCHEAT_DRAWCHEATBOX+1))();
 }
 
-/*
- * @param float x
- * @param float y
- * @param float z
-*/
-void CCheat::UpdateMarkerCoords(float x, float y, float z)
+void ccheat__Draw()
 {
-    CCheat::GetClass()->marker_X = x;
-    CCheat::GetClass()->marker_Y = y;
-    CCheat::GetClass()->marker_Z = z;
-
-    return;
+	return (( void (*)())(getSMM()+ADDR_CCHEAT_DRAW+1))();
 }
-
-/*
- * @param int stepId
-*/
-void CCheat::SetMenuStep(int stepId)
-{
-    CCheat::GetClass()->m_bMenuCurrentStepId = stepId;
-
-    return;
-}
-
-/*
- * @return int
-*/
-int CCheat::GetMenuStep() 
-{
-    return ((int)CCheat::GetClass()->m_bMenuCurrentStepId);
-}
-
-/*
- * @param int 		renderId
- * @param uint8_t 	state
-*/
-void CCheat::ToggleRender(int renderId, uint8_t state)
-{
-    switch(renderId)
-    {
-        case ID_RENDER_CHEATBOX:
-	    CCheat::GetClass()->m_bCheatBoxState = state;
-        break;
-
-        case ID_RENDER_CHEATMENU:
-        CCheat::GetClass()->m_bMenuState = state;
-        break;
-
-        case ID_RENDER_CLICKSENDER:
-        CCheat::GetClass()->m_bClickSenderState = state;
-        break;
-
-        case ID_RENDER_KEYS:
-        CCheat::GetClass()->m_bKeysState = state;
-        break;
-
-        case ID_RENDER_SKINCHANGER:
-        CCheat::GetClass()->m_bSkinChangerState = state;
-        break;
-
-        case ID_RENDER_NOCLIPTOOLS:
-        CCheat::GetClass()->m_bNoClipState = state;
-        break;
-
-        case ID_RENDER_WARP:
-        CCheat::GetClass()->m_bWarpState = state;
-        break;
-    }
-
-    return;
-}
-
-/*
- * @param int patchId
-*/
-void CCheat::ToggleGTAPatch(int patchId)
-{
-    return CCheat::GetClass()->ToggleGTAPatch(patchId);
-}
-
-/*
- * @param int rpcId
-*/
-void CCheat::ToggleRPC(int rpcId)
-{
-    return CCheat::GetClass()->ToggleRPC(rpcId);
-}
-
-/*
- * @param int packetId
-*/
-void CCheat::TogglePacket(int packetId)
-{
-    return CCheat::GetClass()->TogglePacket(packetId);
-}
-
-/*
- * @param int posId
-*/
-#define MIN_POS     0
-#define MAX_POS     55
-#define MARKER_POS  69
-void CCheat::ToggleTeleport(int posId)
-{
-    return CCheat::GetClass()->ToggleTeleport(posId);
-}	
-
-/*
- * @param int cheatId
-*/
-void CCheat::ToggleCheat(int cheatId)
-{
-    return CCheat::GetClass()->ToggleCheat(cheatId);
-}
-
-/*
- * @param int nopId
-*/
-void CCheat::ToggleNOP(int nopId)
-{
-    // not using
-
-    return;
-}
-
-void CCheat::Render() 
-{ 
-    CCheat::GetClass()->RenderCheatBox();
-    CCheat::GetClass()->RenderKeys();
-    CCheat::GetClass()->RenderNoClipTools();
-    CCheat::GetClass()->RenderClickSender();
-    CCheat::GetClass()->RenderCheatMenu();
-    CCheat::GetClass()->RenderSkinChanger();
-    CCheat::GetClass()->RenderWarp();
-
-    if(CCheat::GetClass()->pDialogWindow) 
-        CCheat::GetClass()->pDialogWindow->Render();
-    if(CCheat::GetClass()->pKeyBoard) 
-        CCheat::GetClass()->pKeyBoard->Render();
-
-    return;
-}
-
-// -------------------- EVENTS BEGIN -------------------- //
-
-/*
- * @param int eventId
-*/
-void CCheat::PadEvent(int eventId)
-{
-    return CCheat::GetClass()->PadEvent(eventId);
-}
-
-/*
- * @param uint8_t action
- *
- * @return bool
-*/
-bool onSendTextDraw(uint8_t action)
-{
-	return true;
-}
-
-/*
- * @param uint8_t action
- *
- * @return bool
-*/
-bool onSendMenuRow(uint8_t action)
-{
-	return true;
-}
-
-/*
- * @param uint8_t action
- *
- * @return bool
-*/
-bool onSendSAMPKey(uint8_t action)
-{
-	return true;
-}
-
-/*
- * @param uint8_t action
- *
- * @return bool
-*/
-bool onLegitSelected(uint8_t action)
-{
-	return true;
-}
-
-/*
- * @param uint8_t action
- *
- * @return bool
-*/
-bool onRageSelected(uint8_t action)
-{
-	return true;
-}
-
-/*
- * @param uint8_t action
- *
- * @return bool
-*/
-bool onVisualSelected(uint8_t action)
-{
-	return true;
-}
-
-/*
- * @param uint8_t action
- *
- * @return bool
-*/
-bool onWeaponsSelected(uint8_t action)
-{
-    return true;
-}
-
-/*
- * @param uint8_t action
- *
- * @return bool
-*/
-bool onTeleportsSelected(uint8_t action)
-{
-    return true;
-}
-
-/*
- * @param uint8_t action
- *
- * @return bool
-*/
-bool onGTAPatchesSelected(uint8_t action)
-{
-    return true;
-}
-
-/*
- * @param uint8_t action
- *
- * @return bool
-*/
-bool onSAMPPatchesSelected(uint8_t action)
-{
-    return true;
-}
-
-/*
- * @param uint8_t action
- *
- * @return bool
-*/
-bool onBackPressed(uint8_t action)
-{
-    return true;
-}
-
-// -------------------- EVENTS END -------------------- //
-
-// -------------------- RENDER BEGIN -------------------- //
-
-void CCheat::RenderCheatMenu() { return CCheat::GetClass()->RenderCheatMenu(); }
-void CCheat::RenderCheatBox() { return CCheat::GetClass()->RenderCheatBox(); }
-void CCheat::RenderClickSender() { return CCheat::GetClass()->RenderClickSender(); }
-void CCheat::RenderKeys() { return CCheat::GetClass()->RenderKeys(); }
-void CCheat::RenderNoClipTools() { return CCheat::GetClass()->RenderNoClipTools(); }
-void CCheat::RenderSkinChanger() { return CCheat::GetClass()->RenderSkinChanger(); }
-void CCheat::RenderWarp() { return CCheat::GetClass()->RenderWarp(); }
-
-// -------------------- RENDER END -------------------- //
-
-// -------------------- MENU GENERATOR BEGIN -------------------- //
-
-void CCheat::GenerateBasic() { return CCheat::GetClass()->GenerateBasic(); }
-void CCheat::GenerateLegit() { return CCheat::GetClass()->GenerateLegit(); }
-void CCheat::GenerateRage() { return CCheat::GetClass()->GenerateRage(); }
-void CCheat::GenerateVisual() { return CCheat::GetClass()->GenerateVisual(); }
-void CCheat::GenerateGTAPatches() { return CCheat::GetClass()->GenerateGTAPatches(); }
-void CCheat::GenerateSAMPPatches() { return CCheat::GetClass()->GenerateSAMPPatches(); }
-void CCheat::GenerateTeleports() { return CCheat::GetClass()->GenerateTeleports(); }
-void CCheat::GenerateWeapons() { return CCheat::GetClass()->GenerateWeapons(); }
-void CCheat::GenerateIncomingRPC() { return CCheat::GetClass()->GenerateIncomingRPC(); }
-void CCheat::GenerateOutcomingRPC() { return CCheat::GetClass()->GenerateOutcomingRPC(); }
-void CCheat::GenerateIncomingPackets() { return CCheat::GetClass()->GenerateIncomingPackets(); }
-void CCheat::GenerateOutcomingPackets() { return CCheat::GetClass()->GenerateOutcomingPackets(); }
-
-// -------------------- MENU GENERATOR END -------------------- //
--- main.h
+++ main.h
@@ -1,269 +1,111 @@
 #pragma once
 
-// Render
-#define ID_RENDER_CHEATBOX 						0
-#define ID_RENDER_CHEATMENU						1
-#define ID_RENDER_CLICKSENDER 					2
-#define ID_RENDER_KEYS							3
-#define ID_RENDER_NOCLIPTOOLS 					4
-#define ID_RENDER_SKINCHANGER 					5
-#define ID_RENDER_WARP 							6
+#define MENU_TITLE 							"mod_s0beit_sa"
 
-// Events
-#define ID_EVENT_ONSENDTEXTDRAW 				0
-#define ID_EVENT_ONSENDMENUROW 					1
-#define ID_EVENT_ONSENDSAMPKEY 					2
-#define ID_EVENT_ONLEGITSELECTED 				3
-#define ID_EVENT_ONRAGESELECTED 				4
-#define ID_EVENT_ONVISUALSELECTED 				5
-#define ID_EVENT_ONWEAPONSSELECTED 				6
-#define ID_EVENT_ONTELEPORTSSELECTED 			7
-#define ID_EVENT_ONGTAPATCHESSELECTED 			8
-#define ID_EVENT_ONSAMPPATCHESSELECTED 			9
-#define ID_EVENT_ONBACKPRESSED 					10
+#define FUNC_SET_CHEATS_STATE 				0
+#define FUNC_SET_TELEPORTS_STATE 			1
+#define FUNC_SET_WEAPONS_STATE 				2
+#define FUNC_SET_GTA_PATCHES_STATE  		3
+#define FUNC_SET_SAMP_PATCHES_STATE 		4
+#define FUNC_SET_CHEAT_INVINCIBLE_STATE 	5
+#define FUNC_SET_CHEAT_WALLHACK_STATE 		6
+#define FUNC_SET_CHEAT_NOFALL_STATE 		7
+#define FUNC_SET_CHEAT_FREEZE_STATE 		8
+#define FUNC_SET_CHEAT_INVISIBLE_STATE 		9
+#define FUNC_SET_CHEAT_SURFIST_STATE 		10
+#define FUNC_SET_CHEAT_NOCLIP_STATE 		11
+#define FUNC_SET_CHEAT_FLASH_STATE 			12
+#define FUNC_SET_CHEAT_HPPULSATOR_STATE 	13
+#define FUNC_SET_CHEAT_ARMPULSATOR_STATE 	14
+#define FUNC_SET_CHEAT_JUMPER_STATE 		15
+#define FUNC_SET_CHEAT_BEHIND_STATE 		16
+#define FUNC_SET_CHEAT_ANTISTUN_STATE 		17
+#define FUNC_SET_CHEAT_PLUSCBUG_STATE 		18
+#define FUNC_SET_CHEAT_RVANKA_STATE 		19
+#define FUNC_SET_CHEAT_DAMAGER_STATE 		20
+#define FUNC_SET_CHEAT_SLAPPER_STATE 		21
+#define FUNC_SET_CHEAT_CRASHERA_STATE 		22
+#define FUNC_SET_CHEAT_CRASHERB_STATE 		23
+#define FUNC_SET_CHEAT_CRASHERC_STATE 		24
+#define FUNC_SET_CHEAT_NORELOAD_STATE 		25
+#define FUNC_SET_CHEAT_NOSPREAD_STATE 		26
+#define FUNC_SET_CHEAT_NOBIKEFALL_STATE 	27
+#define FUNC_SET_CHEAT_CARSHOT_STATE 		28
 
-// Pad events
-#define ID_PADEVENT_NONE 						0
-#define ID_PADEVENT_JUMP						1
-#define ID_PADEVENT_FIRE 						2
+// ?!?!?!?!
+#define CHEATS_DISABLED 		0
+#define CHEATS_ENABLED 			1
 
-// Cheat menu step's		
-#define ID_MENU_STEP_TYPES					0
-#define ID_MENU_STEP_LEGIT					1
-#define ID_MENU_STEP_RAGE					2
-#define ID_MENU_STEP_VISUAL					3
-#define ID_MENU_STEP_GTAPATCHES				4
-#define ID_MENU_STEP_SAMPPATCHES			5
-#define ID_MENU_STEP_TELEPORTS				6
-#define ID_MENU_STEP_WEAPONS 				7
-#define ID_MENU_STEP_NOCLIPTOOLS 			8
-#define ID_MENU_STEP_INCOMINGRPC 			9
-#define ID_MENU_STEP_OUTCOMINGRPC 			10
-#define ID_MENU_STEP_INCOMINGPACKETS 		11
-#define ID_MENU_STEP_OUTCOMINGPACKETS 		12
+#define MAIN_STATE 				0
+#define CHEATS_STATE 			1
+#define SAMP_PATCHES_STATE 		2
+#define INCOMING_RPC_STATE 		3
+#define OUTCOMING_RPC_STATE 	4
+#define GTA_PATCHES_STATE 		5
+#define SETTINGS_STATE 			6
+#define TELEPORTS_STATE 		7
+#define WEAPONS_STATE 			8
 
-// Incoming RPC
-#define ID_RPC_DISPLAYGAMETEXT 				0
-#define ID_RPC_SETGRAVITY 					1
-#define ID_RPC_FORCESPAWNSELECTION 			2
-#define ID_RPC_SETPLAYERPOS 				3
-#define ID_RPC_SETCAMERAPOS 				4
-#define ID_RPC_SETCAMERALOOKAT 				5
-#define ID_RPC_SETPLAYERFACINGANGLE 		6
-#define ID_RPC_SETFIGHTINGSTYLE 			7
-#define ID_RPC_SETPLAYERSKIN				8
-#define ID_RPC_APPLYPLAYERANIMATION 		9
-#define ID_RPC_CLEARPLAYERANIMATIONS 		10
-#define ID_RPC_SETSPAWNINFO 				11
-#define ID_RPC_CREATEEXPLOSION 				12
-#define ID_RPC_SETPLAYERHEALTH 				13
-#define ID_RPC_SETPLAYERARMOUR 				14
-#define ID_RPC_SETPLAYERCOLOR 				15
-#define ID_RPC_SETPLAYERNAME 				16
-#define ID_RPC_SETPLAYERPOSFINDZ 			17
-#define ID_RPC_SETPLAYERINTERIOR 			18
-#define ID_RPC_SETMAPICON 					19
-#define ID_RPC_DISABLEMAPICON 				20
-#define ID_RPC_SETCAMERABEHINDPLAYER 		21
-#define ID_RPC_SETPLAYERSPECIALACTION 		22
-#define ID_RPC_TOGGLEPLAYERSPECTATING 		23
-#define ID_RPC_SETPLAYERSPECTATING 			24
-#define ID_RPC_PLAYERSPECTATEPLAYER 		25
-#define ID_RPC_PLAYERSPECTATEVEHICLE 		26
-#define ID_RPC_PUTPLAYERINVEHICLE 			27
-#define ID_RPC_VEHICLEPARAMS 				28
-#define ID_RPC_VEHICLEPARAMSEX 				29
-#define ID_RPC_HAVESOMEMONEY 				30
-#define ID_RPC_RESETMONEY 					31
-#define ID_RPC_LINKVEHICLE 					32
-#define ID_RPC_REMOVEPLAYERFROMVEHICLE 		33
-#define ID_RPC_SETVEHICLEHEALTH 			34
-#define ID_RPC_SETVEHICLEPOS 				35
-#define ID_RPC_SETVEHICLEVELOCITY 			36
-#define ID_RPC_INTERPOLATECAMERA 			37
-#define ID_RPC_ADDGANGZONE 					38
-#define ID_RPC_REMOVEGANGZONE 				39
-#define ID_RPC_FLASHGANGZONE 				40
-#define ID_RPC_STOPFLASHGANGZONE 			41
-#define ID_RPC_CREATEOBJECT 				42
-#define ID_RPC_DESTROYOBJECT 				43
-#define ID_RPC_SETOBJECTPOS 				44
-#define ID_RPC_PLAYSOUND 					45
-#define ID_RPC_SETPLAYERWANTEDLEVEL 		46
-#define ID_RPC_MOVEOBJECT 					47
-#define ID_RPC_RESETPLAYERWEAPONS 			48
-#define ID_RPC_GIVEPLAYERWEAPON 			49
-#define ID_RPC_TOGGLEPLAYERCONTROLLABLE 	50
-#define ID_RPC_SETARMEDWEAPON				51
-#define ID_RPC_SHOWTEXTDRAW 				52
-#define ID_RPC_SHOWMENU 					53
-#define ID_RPC_INITGAME 					54
-#define ID_RPC_SERVERJOIN  					55
-#define ID_RPC_SERVERQUIT 					56
-#define ID_RPC_CLIENTMESSAGE 				57
-#define ID_RPC_CHAT 						58
-#define ID_RPC_REQUESTCLASS 				59
-#define ID_RPC_WEATHER 						60
-#define ID_RPC_REQUESTSPAWN 				61
-#define ID_RPC_WORLDTIME 					62
-#define ID_RPC_SETTIMEEX 					63
-#define ID_RPC_WORLDPLAYERADD 				64
-#define ID_RPC_WORLDPLAYERREMOVE 			65
-#define ID_RPC_SETCHECKPOINT 				66
-#define ID_RPC_DISABLECHECKPOINT 			67
-#define ID_RPC_SETRACECHECKPOINT 			68
-#define ID_RPC_DISABLERACECHECKPOINT 		69
-#define ID_RPC_WORLDVEHICLEADD 				70
-#define ID_RPC_WORLDVEHICLEREMOVE 			71
-#define ID_RPC_ENTERVEHICLE 				72
-#define ID_RPC_EXITVEHICLE 					73
-#define ID_RPC_SHOWDIALOG 					74
-#define ID_RPC_GAMEMODERESTART 				75
-#define ID_RPC_CONNECTIONREJECTED 			76
-#define ID_RPC_CREATEPICKUP 				77
-#define ID_RPC_DESTROYPICKUP 				78
-#define ID_RPC_CREATETEXTLABEL 				79
-#define ID_RPC_DELETETEXTLABEL 				80
-#define ID_RPC_UPDATETEXTLABEL 				81
-#define ID_RPC_UPDATESCORESPINGSIPS 		82
+#define MAX_CHEATS 				100
+#define MAX_RPCS 				100
+#define MAX_PACKETS 			100
+#define MAX_RAKBOTS 			120
 
-// -------------------- not using -------------------- //
-#define ID_RPC_GETCURRENTWEAPON 			83
-#define ID_RPC_GETPLAYERHEALTH				84
-#define ID_RPC_GETPLAYERPOS 				85
-// -------------------- not using -------------------- //
+#define ID_CHEAT_INVINCIBLE 	0
+#define ID_CHEAT_WALLHACK 		1
+#define ID_CHEAT_NOFALL 		2
+#define ID_CHEAT_FREEZE 		3
+#define ID_CHEAT_INVISIBLE 		4
+#define ID_CHEAT_SURFIST 		5
+#define ID_CHEAT_NOCLIP 		6
+#define ID_CHEAT_FLASH 			7
+#define ID_CHEAT_HPPULSATOR 	8
+#define ID_CHEAT_ARMPULSATOR 	9
+#define ID_CHEAT_JUMPER  		10
+#define ID_CHEAT_BEHIND 		11
+#define ID_CHEAT_ANTISTUN 		12
+#define ID_CHEAT_PLUSCBUG 		13
+#define ID_CHEAT_RVANKA 		14
+#define ID_CHEAT_DAMAGER 		15
+#define ID_CHEAT_SLAPPER 		16
+#define ID_CHEAT_CRASHERA 		17
+#define ID_CHEAT_CRASHERB 		18
+#define ID_CHEAT_CRASHERC 		19
+#define ID_CHEAT_NORELOAD 		20
+#define ID_CHEAT_NOSPREAD 		21
+#define ID_CHEAT_NOBIKEFALL 	22
+#define ID_CHEAT_CARSHOT 		23
 
-// Outcoming RPC
-#define ID_RPC_OUTREQUESTSPAWN 				86
-#define ID_RPC_OUTREQUESTCLASS 				87
-#define ID_RPC_OUTDEATH 					88
-#define ID_RPC_OUTENTERVEHICLE 				89
-#define ID_RPC_OUTEXITVEHICLE 				90
-#define ID_RPC_OUTSETINTERIORID 			91
-#define ID_RPC_OUTSPAWN 					92
-#define ID_RPC_OUTGIVETAKEDAMAGE 			93
+// api stuff
+char* 	GetAPIVersion();
+char* 	GetSMMVersion();
+int 	GetCoreProtocol();
+void 	InitProtocol();
 
-// unknown RPC
-#define ID_RPC_UNKNOWN 						255
+uintptr_t getSMM();
 
-// Packet's
-#define ID_PACKET_AUTHKEY 						0
-#define ID_PACKET_DISCONNECTIONNOTIFICATION 	1
-#define ID_PACKET_CONNECTIONLOST 				2
-#define ID_PACKET_CONNECTIONSUCCEEDED 			3
-#define ID_PACKET_PLAYERSYNC 					4
-#define ID_PACKET_VEHICLESYNC 					5
-#define ID_PACKET_PASSENGERSYNC 				6
-#define ID_PACKET_MARKERSSYNC 					7
-#define ID_PACKET_AIMSYNC 						8
-#define ID_PACKET_BULLETSYNC 					9
-#define ID_PACKET_UNOCCUPPIEDSYNC 				10
-#define ID_PACKET_TRAILERSYNC 					11
-#define ID_PACKET_WEAPONDATA 					12
-#define ID_PACKET_PLAYERDATA 					13
-#define ID_PACKET_VEHICLEDATA 					14
-#define ID_PACKET_UNKNOWN 						255
+// ccheat class :: funcs
+void ccheat__menu_item_add(char* itemName, int funcId, int menuType);
+bool ccheat__IsEnabled() 												{ return m_bEnabled; }
+void ccheat__SetEnabled(bool state) 									{ m_bEnabled = state; }
+void ccheat__SetMenuState(int stateId) 									{ menuState = stateId; }
+bool ccheat__GetMenuState() 											{ return menuState; }
+bool ccheat__GetCheatState(int cheatId) 								{ return cheatsState[cheatId]; }
+bool ccheat__GetRPCState(int rpcId) 									{ return rpcsState[rpcId]; }
+bool ccheat__GetPacketState(int pktId) 									{ return packetsState[pktId]; }
+void ccheat__SetCheatState(int cheatId, bool state) 					{ cheatsState[cheatId] = state; }
+void ccheat__SetRPCState(int rpcId, bool state)							{ rpcsState[rpcId] = state; }
+void ccheat__SetPacketState(int pktId, bool state) 						{ packetsState[pktId] = state; }
+void ccheat__makerakbot(const char* name, int id);
+void ccheat__disablerakbot(int id);
+void ccheat__rakbot_follow(int botId, unsigned short entityId);
+void ccheat__rakbot_setcheatstate(int cheatId, bool state);
+void ccheat__DrawCheatBox();
+void ccheat__Draw();
 
-// NOP's (not using)
-#define ID_NOP_SETENGINESTATE 				0 
-
-// Cheats
-#define ID_CHEAT_INVINCIBLE 				0
-#define ID_CHEAT_WALLHACK 					1
-#define ID_CHEAT_FREEZE 					2
-#define ID_CHEAT_BEHIND 					3
-#define ID_CHEAT_NOFALL 					4
-#define ID_CHEAT_INVISIBLE 					5
-#define ID_CHEAT_FLASH 						6
-#define ID_CHEAT_AIRBREAK 					7
-#define ID_CHEAT_HPPULSATOR 				8
-#define ID_CHEAT_ARMPULSATOR 				9
-#define ID_CHEAT_HIGHJUMP 					10
-#define ID_CHEAT_SURF 						11
-#define ID_CHEAT_NOCLIP 					12
-
-// GTA Patches
-#define ID_GTAPATCH_MONEY 					0
-#define ID_GTAPATCH_NITRO 					1
-#define ID_GTAPATCH_CLOCK 					2
-#define ID_GTAPATCH_LOCKTIME 				3
-#define ID_GTAPATCH_LOCKWEATHER 			4
-#define ID_GTAPATCH_RADAR 					5
-#define ID_GTAPATCH_FASTFIRE 				6
-#define ID_GTAPATCH_CARINVISIBLE 			7
-#define ID_GTAPATCH_DRIVEBY 				8
-#define ID_GTAPATCH_SUICIDE 				9
-#define ID_GTAPATCH_JETPACK 				10
-#define ID_GTAPATCH_BLOWUPCARS 				11
-#define ID_GTAPATCH_INFINITYSPRINT 			12
-#define ID_GTAPATCH_CJWALK 					13
-
-// PC keyboard buttons
-#define	ID_KEY_ALT 							1024
-#define	ID_KEY_TAB 							1
-#define	ID_KEY_C 							2
-#define	ID_KEY_LCTRL 						4
-#define	ID_KEY_SPACE 						8
-#define	ID_KEY_ENTER 						16
-#define	ID_KEY_LSHIFT 						32
-#define	ID_KEY_E 							64
-#define	ID_KEY_RMB 							128
-#define	ID_KEY_Q 							256
-#define	ID_KEY_TWO 							320
-#define	ID_KEY_TWOS 						512
-#define	ID_KEY_Y 							65536
-#define	ID_KEY_N 							131072
-
-extern char* GetAPIVersion();
-extern char* GetSMMVersion();
-extern int GetCoreProtocol();
-extern void InitProtocol();
-
-class CCheat {
-
-private:
-	CCheat* GetClass();
-
-public:
-	void Process();
-
-	float GetNoClipSpeed();
-	void SetNoClipSpeed(float speed);
-
-	void UpdateMarkerCoords(float x, float y, float z);
-
-	void ToggleRender(int renderId, uint8_t state);
-	void ToggleGTAPatch(int patchId);
-	void ToggleRPC(int rpcId);
-	void TogglePacket(int packetId);
-	void ToggleTeleport(int posId);
-	void ToggleCheat(int cheatId);
-	void ToggleNOP(int nopId);
-
-	void SetMenuStep(int stepId);
-	int GetMenuStep();
-
-	void GenerateBasic();
-	void GenerateLegit();
-	void GenerateRage();
-	void GenerateVisual();
-	void GenerateGTAPatches();
-	void GenerateSAMPPatches();
-	void GenerateTeleports();
-	void GenerateWeapons();
-	void GenerateIncomingRPC();
-	void GenerateOutcomingRPC();
-	void GenerateIncomingPackets();
-	void GenerateOutcomingPackets();
-
-	void Render();
-	void RenderCheatMenu();
-	void RenderCheatBox();
-	void RenderClickSender();
-	void RenderKeys();
-	void RenderNoClipTools();
-	void RenderSkinChanger();
-
-	void PadEvent(int eventId);
-	
-};
+// ccheat class :: vars
+bool m_bEnabled;
+bool cheatsState[MAX_CHEATS];
+bool rpcsState[MAX_RPCS];
+bool packetsState[MAX_PACKETS];
+int	 menuState;
